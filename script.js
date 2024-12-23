305c44f68bf8c96163617d7a253b26ae1f219c66
Giulio Rossini <giulio.rossini03@gmail.com>
2024-12-23 22:54:42 +0100

    modifiche

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ document.querySelector('.btn').addEventListener('click', function() {
         block: 'start'
     });
 });
+// Ottieni tutti i prodotti nella sezione 'trend'
+const products = document.querySelectorAll('.trend .product');
+
+// Aggiungi un event listener a ciascun prodotto
+products.forEach(product => {
+    product.addEventListener('click', function() {
+        // Seleziona l'immagine all'interno del prodotto cliccato
+        const img = product.querySelector('img');
+        
+        
+        // Aggiungi la classe 'rotate' per far girare l'immagine
+        img.classList.toggle('rotate');
+
+        // Facoltativo: Se vuoi far tornare l'immagine alla posizione iniziale dopo un po' di tempo, rimuovi la classe
+        setTimeout(() => {
+            img.classList.remove('rotate');
+        }, 1000); // 1000 ms (1 secondo) dopo il click
+    });
+});
